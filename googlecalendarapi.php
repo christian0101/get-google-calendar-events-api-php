62572d464a3d78458ff8e0a099f8292dd4c001d9
Needy <christian0101@users.noreply.github.com>
2019-09-05 16:21:53 +0300

    comments

--- googlecalendarapi.php
+++ googlecalendarapi.php
@@ -16,12 +16,19 @@
 // https://github.com/google/google-api-php-client
 include(__DIR__.'/vendor/autoload.php');
 
-/*
-   The calendar id, found in calendar settings.
-   If your calendar is through google apps
-   you may need to change the central sharing settings. the calendar for this script
-   must have all events viewable in sharing settings.
-*/
+/**
+ * get_upcoming_events gets upcoing events from a google calendar by id.
+ *
+ * @param $_G_API GOOGLE PUBLIC API key server,
+ *                from DEVELOPERS.GOOGLE.COM for API.
+ * @param $_calendarId The calendar id, found in calendar settings.
+ *        If your calendar is through google apps
+ *        you may need to change the central sharing settings.
+ *        The calendar for this script must have all events viewable in
+ *        sharing settings.
+ * @param $_max_events maximum number of events to be requested, default (4).
+ * @return html formated event(s).
+ */
 function get_upcoming_events($_G_API, $_calendarId, $_max_events = 4) {
     // used in Greece, thus the timezone and locale
     date_default_timezone_set('Europe/Athens');
@@ -32,8 +39,6 @@ function get_upcoming_events($_G_API, $_calendarId, $_max_events = 4) {
     // New Google client
     $client = new Google_Client();
     $client->setApplicationName("Calendar of Upcoming Events");
-
-    // DEVELOPERS.GOOGLE.COM for API
     $client->setDeveloperKey($_G_API);
     $cal = new Google_Service_Calendar($client);
 
